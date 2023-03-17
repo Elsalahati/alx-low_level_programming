26bb3e01ea5b5622e56eb0f95760edbf28827621
Elsalahati <hazem.elsalahaty@gmail.com>
2023-03-16 18:35:30 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *  main - Outputting using puts() function instead of printf()
+ * Return: (0) if sucess
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
