0995d89b0610d8f18f4cef7bcc956f1597eef8ef
Elsalahati <hazem.elsalahaty@gmail.com>
2023-03-17 19:13:52 -0700

    putchar /0/

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - main function of the program
+ * description - the starting point of the program
+ * Return: (0) success
+ **/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
