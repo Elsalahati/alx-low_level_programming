f8d010f651d85d7d9cde4c35761c6c1168a884de
Elsalahati <hazem.elsalahaty@gmail.com>
2023-03-21 12:54:35 -0700

    multiple putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - main function of the program
+ * description - the starting point of the program
+ * Return: (0) success
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
+
