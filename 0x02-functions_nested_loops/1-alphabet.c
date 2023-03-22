22bcfef0f7534b00118ae68baae54cec5c542c0d
Elsalahati <hazem.elsalahaty@gmail.com>
2023-03-22 13:30:51 -0700

    main h file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
-/* betty style doc for function main goes there */
+#include "main.h"
+
 /**
  * print_alphabet - print the alphabet in lower case
- * description - the starting point of the program
- * Return: (0) success
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
@@ -12,8 +11,7 @@ void print_alphabet(void)
 
 	for (p = 'a'; p <= 'z'; p++)
 	{
-		putchar(p);
-		putchar('\n');
+		_putchar(p);
 	}
-	return (0);
+	_putchar('\n');
 }
