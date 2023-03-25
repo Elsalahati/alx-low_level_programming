4d2c1628c4195e04e974540d21c532dfa9684614
Elsalahati <hazem.elsalahaty@gmail.com>
2023-03-24 17:59:10 -0700

    Inverted Triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - draws a triangle on the terminal
+ * @size: size of the triangle
+ * Return: triangle size up to the number of # and print a new line otherwise
+ */
+
+void print_triangle(int size)
+{
+	int rows;
+	int columns;
+	int sign;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (rows = 0; rows < size; rows++)
+		{
+			for (columns = size - rows; columns > 0; columns--)
+			{
+				_putchar(32);
+			}
+				for (sign = 0; sign <= rows ; sign++) 
+				{
+					_putchar('#');
+				}
+		_putchar('\n');
+		}
+	}
+}
