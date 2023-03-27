da6193a6e9973cf86772ea181fbf7dc70853a6c8
Elsalahati <hazem.elsalahaty@gmail.com>
2023-03-27 15:11:59 -0700

    pointers first use

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - checks if a lower case character is found or not
+ * @n: value pointed to by the pointer
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
