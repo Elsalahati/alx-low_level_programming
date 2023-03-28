bda64c205469c404478ba99a2a370a1ac4a7cd4b
Elsalahati <hazem.elsalahaty@gmail.com>
2023-03-28 08:32:46 -0700

    printing out even characters only

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character
+ * @str: characters to be printed
+ * Return: no return
+ **/
+
+void puts2(char *str)
+{
+	int p;
+	
+	for (p = 0; str[p] != '\0'; p++)
+	{
+		if (str[p] % 2 == 0)
+		_putchar(str[p]);
+	}
+	_putchar('\n');
+}
