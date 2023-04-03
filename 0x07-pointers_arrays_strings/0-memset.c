ef198fea0d876972cc01ef74e18e21feaa8fa56e
Elsalahati <hazem.elsalahaty@gmail.com>
2023-04-03 09:06:39 -0700

    adding the files first

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @b: constant byte b
+ * @n: number of bytes in memory
+ * @s: pointer pointing to the memory area
+ * Return: s (pointer to memory area)
+ **/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+	int k;
+
+	_memset(b, );
+	return (s);
+}
