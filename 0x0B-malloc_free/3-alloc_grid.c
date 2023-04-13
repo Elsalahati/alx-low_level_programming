8df7c0ce1cd3a29653076313192751def0882893
Elsalahati <hazem.elsalahaty@gmail.com>
2023-04-13 01:48:37 +0000

    2D Array of Integers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,21 +11,42 @@
 
 int **alloc_grid(int width, int height)
 {
-	char arr[][];
-	int w, x, y, z;
 	int **arr;
+	int w, x, y, z;
 
-	if (width <= 0 || height <= 0)
+	if (width <= 0)
 	{
 		return ('\0');
 	}
-	arr = malloc(sizeof(int*) * height);
-	if (arr == '\0')
+	if (height <= 0)
+	{
+		return ('\0');
+	}
+	arr = malloc(sizeof(int *) * height);
+	if (arr == NULL)
 	{
 		free(arr);
 		return ('\0');
 	}
-	
-	p[w] = '\0';
-	return (p);
+	for (x = 0; x < height; x++)
+	{
+		arr[x] = malloc(sizeof(int) * width);
+		if (arr[x] == NULL)
+		{
+			for (y = x; y > -1; y--)
+			{
+				free(arr[y]);
+			}
+			free(arr);
+			return ('\0');
+		}
+	}
+	for (z = 0; z < height; z++)
+	{
+		for (w = 0 ; w < width; w++)
+		{
+			arr[z][w] = 0;
+		}
+	}
+	return (arr);
 }
