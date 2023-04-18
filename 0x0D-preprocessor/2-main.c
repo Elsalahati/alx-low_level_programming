f3585c8c948eff24fe05ddb2f93a8321c9d97c40
Elsalahati <hazem.elsalahaty@gmail.com>
2023-04-18 05:59:31 +0000

    defining Pi

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* main - prints out the name of the file
+*
+* Return: 0 always (success)
+**/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
