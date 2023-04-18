213f7bea89eeb50b4ad964d927eedaaf30229d19
Elsalahati <hazem.elsalahaty@gmail.com>
2023-04-18 10:08:39 +0000

    A poppy appeared!

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog 
+/**
+ * struct dog - defining a new dog
+ * @name: name of the dog
+ * @age: its age
+ * @owner: his owner
+ **/
+
+struct dog
 {
 	char *name;
 	float age;
