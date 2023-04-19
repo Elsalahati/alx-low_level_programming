af7ab6d2a59f08a62c01251436c3c055de8f1d77
Elsalahati <hazem.elsalahaty@gmail.com>
2023-04-19 21:07:43 +0000

    Executing a certain function pointed to by action

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_iterator - executes a function pointed to by (action)
+ * @array: input array
+ * @size: size of the array
+ * @action: pointer pointing to an input function
+ * Return: no return
+ **/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int k;
+
+	if (array != NULL || size > 0 || action != NULL)
+	{
+		for (k = 0; k < size; k++)
+		{
+			action(array[k]);
+		}
+	}
+	else
+	{
+		return;
+	}
+}
