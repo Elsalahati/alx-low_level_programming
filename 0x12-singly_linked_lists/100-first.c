7ce2dd642e58d9f8f1c915d00e72fd334214fcfa
Elsalahati <hazem.elsalahaty@gmail.com>
2023-04-29 21:20:01 +0000

    not working

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include "lists.h"
 
+void SentenceBefore(void) __attribute__((constructor));
+
 /**
- * printing a given sentence before main
+ * SentenceBefore - prints a sentence before main function
+ * Return: no return
  **/
 
-void sentence(void)
+void SentenceBefore(void)
 {
-	printf("You're beat! and yet, you must
-allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+"I bore my house upon my back!\n");
 }
