bbceef7d997ff889d2e05e8939fcd7da96ef5157
Elsalahati <hazem.elsalahaty@gmail.com>
2023-04-29 19:24:48 +0000

    printing a sentence

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * printing a given sentence before main
+ **/
+
+void sentence(void)
+{
+	printf("You're beat! and yet, you must
+allow,\nI bore my house upon my back!\n");
+}
